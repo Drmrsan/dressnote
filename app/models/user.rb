364a7074eced6d72cb24a47c7184f039967794eb
Gor@n <drmrsan@gmail.com>
2016-05-19 13:50:33 +0000

    Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:google_oauth2]
          
-    def self.from_omniouth(auth)
+    def self.from_omniauth(auth)
         where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
             user.provider = auth.provider
             user.uid      = auth.uid
